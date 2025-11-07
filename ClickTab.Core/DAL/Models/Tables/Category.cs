b663aae48692cd16535dcddd6c994c670f47101c
giuver71 <giuver71@gmail.com>
2025-11-07 16:47:05 +0100

    Category Update

--- ClickTab.Core/DAL/Models/Tables/Category.cs
+++ ClickTab.Core/DAL/Models/Tables/Category.cs
@@ -12,7 +12,11 @@ namespace ClickTab.Core.DAL.Models.Tables
         public int ID { get ; set ; }
         public string Description { get; set; }
         public string Code { get; set; }
-
+        public decimal Fee { get; set; }
+        public bool IsFiscal { get; set; }
+        public string Department { get; set; }
+        public bool Negative { get; set; }
+        public bool FeeOnPurchasePrice { get; set; }
         public int FK_InsertUser { get ; set ; }
         public DateTime InsertDate { get ; set ; }
         public int FK_UpdateUser { get ; set ; }
--- ClickTab.Web/Mappings/ModelsDTO/Tables/CategoryDTO.cs
+++ ClickTab.Web/Mappings/ModelsDTO/Tables/CategoryDTO.cs
@@ -7,6 +7,12 @@ namespace ClickTab.Web.Mappings.ModelsDTO.Tables
         public int ID { get; set; }
         public string Description { get; set; }
         public string Code { get; set; }
+        public decimal Fee { get; set; }
+        public bool IsFiscal { get; set; }
+        public string Department { get; set; }
+        public bool Negative { get; set; }
+        public bool FeeOnPurchasePrice { get; set; }
+        public int FK_InsertUser { get; set; }
 
         public int FK_UpdateUser { get; set; }
         public DateTime? UpdateDate { get; set; }
